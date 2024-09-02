6e1ba878bf2b3d8ee9ecaf98a7bbd07338b05604
ridhdhi2510 <162030525+ridhdhi2510@users.noreply.github.com>
2024-09-02 11:39:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ app.all("*" , (req , res , next) =>{
 app.use((err , req , res , next) =>{
      let{statusCode=500 , message = "Something went Wrong"} = err;
      res.status(statusCode).render("error.ejs" , {message})
-     res.status(statusCode).send(message);
+     //res.status(statusCode).send(message);
 });
 
 
